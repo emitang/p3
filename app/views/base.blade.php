3d7988055b48d1f5f01b0272664788fd5c49a57a
Emily Tang <emitang@mit.edu>
2014-10-24 20:51:04 -0400

    first commit

---
+++ app/views/base.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>@yield('title','Developer's Best Friend')</title>
+	<link rel="stylesheet" href="/public/d-b-f.css" type="text/css">
+
+	@yield('head')
+
+</head>
+<body>
+	<div id="top-wrapper">
+		<h1>Developer's Best Friend</h1>
+	</div>
+	@yield('content')
+
+	@yield('body')
+	<small><a href=""
+</body>
